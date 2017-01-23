b9085fcf8c4efc20a4ffe972cde918ff2d74f01e
David Quisenberry and Jason Brown <jason@>
2017-01-23 10:11:59 -0800

    create basic html forms and imput and UI and Business logic to accomplish spec 1: show item.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,21 @@
-function Place(location, landmark, time, notes) {
-  this.loc = location;
-  this.landmark = landmark;
-  this.time = time;
-  this.notes = notes;
+function Todo(item, date, priority){
+  this.item = item;
+  this.dueDate = date;
+  this.priority = priority;
 }
-
-// Place.prototype.destination = function() {
-//   return.this.local + " " + this.
-// }
 
 //UI logic
 
-$(document).ready(function() {
-  $("form#new-trip").submit(function(event) {
+$(document).ready(function(){
+  $("form#to-do").submit(function(event){
     event.preventDefault();
 
-    var inputtedLocation = $("input#new-destination").val();
-    var inputtedLandmark = $("input#new-landmark").val();
-    var inputtedTime = $("input#new-time").val();
-    var inputtedNotes = $("input#new-notes").val();
+    var getItem = $("#new-item").val();
+    var getDate = $("#date-due").val();
+    var getImportance = $("input:radio[name=importance].checked").val();
 
-    var newTrip = new Place(inputtedLocation, inputtedLandmark, inputtedTime, inputtedNotes);
+    var newItem = new Todo(getItem, getDate, getImportance);
 
-    $("ul#trips").append("<li class='trip'>" + newTrip.loc + "</li>");
-
-    $(".trip").last().click(function() {
-      $("#show-trip").show();
-      $("#trip-name").text(newTrip.loc);
-      $(".dates-insert").text(newTrip.time);
-      $(".landmark-insert").text(newTrip.landmark);
-      $(".notes-insert").text(newTrip.notes);
-    });
+    $("ul#todo-list").append("<li class='item'>" + newItem.item + "</li>");
   });
 });
