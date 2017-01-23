b77dbe284505439d210baf11e0676d2b9a85da36
David Quisenberry and Jason Brown <david@>
2017-01-23 11:16:11 -0800

    add checkboxes with unique ids to task objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-function Todo(item, date, priority, notes){
+function Todo(item, date, priority, notes, counter){
   this.item = item;
   this.dueDate = date;
   this.priority = priority;
   this.notes = notes;
+  this.unique = "item" + counter;
 }
 Todo.prototype.getDetails = function() {
   return [this.item, this.dueDate, this.priority, this.notes];
@@ -11,25 +12,29 @@ Todo.prototype.getDetails = function() {
 //UI logic
 
 $(document).ready(function(){
+  //local variable to UI logic to keep track of objects for use in checkbox creation
+  var counter = 1;
   $("form#to-do").submit(function(event){
     event.preventDefault();
-
     var getItem = $("#new-item").val();
     var getDate = $("#date-due").val();
     var getImportance = $("input:radio[name=importance].checked").val();
     var getNotes = $("textarea").val();
 
-    var newItem = new Todo(getItem, getDate, getImportance, getNotes);
+    var newItem = new Todo(getItem, getDate, getImportance, getNotes, counter);
 
-    $("ul#overdue-list").append("<li class='item'>" + newItem.item + "</li>");
+    $("ul#overdue-list").append("<li class='item'>" + newItem.item + "<input type='checkbox' id='" + newItem.unique + "'</li>");
 
     $(".item").last().click(function() {
       $("#details").text("");
+      $("#show-details").show();
       var details = newItem.getDetails();
       details.forEach(function(detail){
         $("#details").append("<p>" + detail + "</p>");
       });
 
     });
+    // increase counter for each submit click for internal tracking on each object created
+    counter += 1;
   });
 });
