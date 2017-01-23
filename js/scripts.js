38ce9ce3789cf1f10f19f4da3caf94ae80a5a9c8
David Quisenberry and Jason Brown <david@>
2017-01-23 10:44:13 -0800

    add for loop and prototype for processing user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
-function Todo(item, date, priority){
+function Todo(item, date, priority, notes){
   this.item = item;
   this.dueDate = date;
   this.priority = priority;
+  this.notes = notes;
+}
+Todo.prototype.getDetails = function() {
+  return [this.item, this.dueDate, this.priority, this.notes];
 }
 
 //UI logic
@@ -13,9 +17,15 @@ $(document).ready(function(){
     var getItem = $("#new-item").val();
     var getDate = $("#date-due").val();
     var getImportance = $("input:radio[name=importance].checked").val();
+    var getNotes = $("textarea").val();
 
-    var newItem = new Todo(getItem, getDate, getImportance);
+    var newItem = new Todo(getItem, getDate, getImportance, getNotes);
 
     $("ul#todo-list").append("<li class='item'>" + newItem.item + "</li>");
+
+    var details = newItem.getDetails();
+    details.forEach(function(detail){
+      $("#details").append("<p>" + detail + "</p>");
+    });
   });
 });
